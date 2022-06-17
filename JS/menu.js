74a8d89434f731080340cd2ce25fcadfb405e9be
FelpsDS <84294217+felpsds@users.noreply.github.com>
2022-06-17 10:04:18 -0300

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -2,7 +2,29 @@ function saveCookies(option){
     document.getElementById("lgpd_cookie").style.display = "none";
     
     if(option = true){
-        document.cookie = "cookie = " + option + ";path=/";
+        document.cookie = "cookieAllow= " + option + ";" + ";path=/";
         alert(document.cookie)
     }
+}
+function checkCookie(){
+    let user = getCookie("cookieAllow");
+    if(user != ""){
+        document.getElementById("lgpd_cookie").style.display = "none";
+    }
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
 }
