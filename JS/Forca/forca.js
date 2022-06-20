48b2b5d3a5f0bab4db0ffb930a7dbdafd90abdff
FelpsDS <84294217+felpsds@users.noreply.github.com>
2022-06-20 11:22:25 -0300

    Update forca.js

--- JS/Forca/forca.js
+++ JS/Forca/forca.js
@@ -1,4 +1,3 @@
-import {getCookie} from '../Forca/words.js';
 const Keyboard = {
 
     elements: {
@@ -128,3 +127,18 @@ function verify(key){
     var contain = item.indexOf(key);
     run(contain,key,indexes);
 };
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
