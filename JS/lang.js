5d9e44a09618a8932095d9b983ccf82eeaecf9a2
FelpsDS <84294217+felpsds@users.noreply.github.com>
2022-06-20 10:28:20 -0300

    Update lang.js

--- JS/lang.js
+++ JS/lang.js
@@ -2,6 +2,3 @@ function getLang(){
     var lang = document.querySelector("select#lang-switch").value;
     return lang;
 }
-window.addEventListener("DOMContentLoaded", function () {
-    lang = getLang();
-});
