6dc4050d4847fbb21d15681a8900057d45620f57
FelipeDS <84294217+felpsds@users.noreply.github.com>
2022-07-26 13:33:03 -0300

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -11,7 +11,7 @@ async function animation(){
         frase = frases[j];
         size = frases[j].length;
         for(let i = 0; i < size; i++){
-            second = Math.floor(Math.random() * 121) + 60;
+            second = Math.floor(Math.random() * 121.) + 60;
             await sleep(second);
             letras = letras + frase[i];
             local.innerHTML = letras;
@@ -20,11 +20,37 @@ async function animation(){
     await sleep(200);
     document.getElementById("contact").style.opacity = 1
     document.getElementById("resume").style.opacity = 1
-    
-
 }
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function changeLang(lang){
+    menu = document.querySelectorAll(".menubtn");
+
+    switch (lang){
+        case "pt_br":
+            fetch("language.json")
+                .then(Response => Response.json())
+                .then(data => {
+                    for(i = 0; i<5; i++){
+                       toreplace = data.pt_br.header[i];
+                        menu[i].innerHTML = toreplace;
+                    }
+        })
+        break;
+
+        case "en":
+            fetch("language.json")
+                .then(Response => Response.json())
+                .then(data => {
+                for(i = 0; i<5; i++){
+                    toreplace = data.en.header[i];
+                    menu[i].innerHTML = toreplace;
+                }
+
+        })
+        break;
+    }
 }
 
