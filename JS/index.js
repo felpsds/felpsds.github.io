f751c18de55d588c82fbc454c2d05ca593c22c88
FelipeDS <84294217+felpsds@users.noreply.github.com>
2022-07-26 10:37:27 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+async function animation(){
+    var frases = [
+        "",
+        "Oi, meu nome é",
+        "Felipe dos Santos",
+        "Eu sou um desenvolvedor Front-End"
+    ]
+    for(let j = 1; j < 4; j++){
+        letras = '';
+        local = document.querySelector(".p"+j);
+        frase = frases[j];
+        size = frases[j].length;
+        console.log(size);
+        for(let i = 0; i < size; i++){
+            second = Math.floor(Math.random() * 200) + 85;
+            await sleep(second);
+            console.log(second);
+            letras = letras + frase[i];
+            local.innerHTML = letras;
+        }
+    }
+    await sleep(200);
+    document.getElementById("contact").style.opacity = 1
+    document.getElementById("resume").style.opacity = 1
+    
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
