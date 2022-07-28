c4f49479c8b134a0127dce53d235dfd6a6481dbc
FelipeDS <84294217+felpsds@users.noreply.github.com>
2022-07-28 13:29:23 -0300

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -78,3 +78,34 @@ function changeLang(lang){
     }
 }
 
+function saveCookies(option){
+    document.getElementById("lgpd_cookie").style.display = "none";
+    if(option == true){
+        document.cookie = "cookieAlert= " + option + ";" + ";path=/";
+    } else {
+        console.log("Not Confirmed!")
+    }
+}
+function checkCookie(){
+    user = getCookie("cookieAlert"); 
+    if(user == ''){
+        document.getElementById("lgpd_cookie").style.display = "flex";
+    }
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
