cbb5232c10e4091a6c281877d3e52232e692a7d0
FelipeDS <84294217+felpsds@users.noreply.github.com>
2022-08-04 10:04:24 -0300

    Add files via upload

--- sg/JS/Ttt/ttt.js
+++ sg/JS/Ttt/ttt.js
@@ -31,36 +31,97 @@ async function create(){
     for(let i=0; i < 3; i++){
         for(let j=0; j < 3; j++){
             colunas[i][j].addEventListener('click', function handleClick() {
-                console.log("O botao clicado foi: linha " + (j+1) + " coluna " + (i+1));
                 check(i+1,j+1,p1,p2);
             });
         }
     }
 }
-round = "p1";
+round = "p1";jogada = 0;
+map=[
+    ["","",""],
+    ["","",""],
+    ["","",""]
+];
 async function check(c,l,p1,p2){
     botao = document.querySelector(".l"+l).querySelector(".c"+c);
     dis = document.querySelector(".l"+l).querySelector(".c"+c).disable;
-    console.log(dis)
     if(dis != true){
         switch (round){
             case "p1":
                 document.querySelector(".l"+l).querySelector(".c"+c).disable = true;
                 botao.style.backgroundImage = "url('/sg/Img/"+ p1+ ".svg')"
                 round  = "p2"
+                jogada++;
+                map[l-1][c-1] = p1;
+                winnerCheck(jogada,map,p1);
             break
             case "p2":
                 document.querySelector(".l"+l).querySelector(".c"+c).disable = true;
                 botao.style.backgroundImage = "url('/sg/Img/"+ p2+ ".svg')"
                 round = "p1";
+                jogada++;
+                map[l-1][c-1] = p2;
+                winnerCheck(jogada,map,p2);
             break
+            
+        }
+        
+    }  
+}
+win = false;
+function winnerCheck(jogada,map,player){
+    player = player.toUpperCase();
+    if(jogada > 4 & jogada < 10){
+        for(l=0;l<3;l++){
+            // ---
+            if(map[l][0] != "" && map[l][1] != ""){
+                if(map[l][0] == map[l][1] & map[l][1] == map[l][2]){
+                    console.log("Venceu!")
+                    win = true
+                }
+            }
+            // |||
+            if(map[0][l] != "" && map[1][l] != ""){
+                if(map[0][l] == map[1][l] & map[1][l] == map[2][l]){
+                    console.log("Venceu!")
+                    win = true
+                }
+            }
+            // /
+            if(map[0][2] != "" && map[2][0] != ""){
+                if(map[0][2] == map[1][1] && map[1][1] == map[2][0]){
+                    console.log("Venceu!")
+                    win = true
+                }
+            }
+            // \
+            if(map[0][0] != "" && map[2][2] != ""){
+                if(map[0][0] == map[1][1] && map[1][1] == map[2][2]){
+                    console.log("Venceu!")
+                    win = true
+                }
+            }
+        }
+
+        if(win == true){
+            gameOvers(true,player);
+        } else if (win == false & jogada == 9){
+            gameOvers(false,player);
         }
     }
+}
+function gameOvers(state,p){
+    windiv = document.getElementById("win");
+    document.getElementById("gameOver").style.display = "flex";
+    if(state == true){
+        windiv.innerText = "O ganhador foi: " + p;
+        windiv.style.display = "flex";
 
-    
+    } else {
+        document.getElementById("draw").style.display = "flex";
+    }
     
 }
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function restart(){
+    location.reload();
 }
